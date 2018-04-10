af6a9e453896216b6c7bd9da76fcf522b03553e6
TiagoEduardo <tiago.eduardo@outlook.com.br>
2018-04-10 09:42:04 -0300

    Primeiro commit. Novos arquivos.

---
+++ app.js
@@ -0,0 +1,18 @@
+$app = angular.module('labApp', []);
+$app.controller('labAppController', function($scope) {
+	
+	$scope.message = "Catalogo";
+	
+	$scope.title = "Lista de contato";
+	
+	$scope.team = [{name:"Cavalcante", hability:"22"},
+						{name:"Thomaz", hability:"28"},
+						{name:"Alfredo", hability:"30"}];
+	
+	$scope.addMember = function($member){
+		$scope.team.push(angular.copy($member));
+	}
+});
+$app.controller('labFootController', function($scope){
+	$scope.footer = "@Todos os direitos reservados.";
+});
